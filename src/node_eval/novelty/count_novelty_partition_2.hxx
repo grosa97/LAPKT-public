00be9561e011a51dcb144c30e05443bca2e1f3ca
Ubuntu <ubuntu@lapkt-3.novalocal>
2023-12-01 11:51:29 +0000

    first iteration max count continuous with po

--- src/node_eval/novelty/count_novelty_partition_2.hxx
+++ src/node_eval/novelty/count_novelty_partition_2.hxx
@@ -175,49 +175,52 @@ namespace aptk
 				// if (temp < -0.3)
 				// 	h_val += temp;
 			
+                                if(m_1 == 0)
+					h_val = 1;
+				else
+					h_val = m_2 + 2;
 
-
-				// if (m_1 == 0)
+				//if (m_1 == 0)
 				// 	h_val = 1;
 				// // else if (tuple_count == 1)
 				// // 	m = 2;
-				// // else if (m_2 == 0 && m_1 <= 2)
-				// // 	h_val = 2;
-				// else if (m_2 == 0)
+				 //else if (m_2 == 0 && m_1 <= 2)
 				// 	h_val = 2;
-				// else if (m_2 == 5)
+				 //else if (m_2 == 0)
 				// 	h_val = 3;
+				 //else if (m_2 == 5)
+				 //	h_val = 4;
 				// // else if (tuple_count <= 20)
 				// // 	m = 5;
 				// // else if (m_2 <= 5)
 				// // 	h_val= 5;
 				// else if (m_2 <= 10)
+				// 	h_val = 4;
+				 //else if (m_2 <= 100)
 				// 	h_val = 5;
-				// else if (m_2 <= 100)
-				// 	h_val = 9;
-				// else if (m_2 <= 200)
-				// 	h_val = 17;
-				// else
-				// 	h_val = 33;
+				 //else if (m_2 <= 200)
+				 //	h_val = 6;
+				 //else
+				// 	h_val = 6;
 
-				if (m_1 == 0)
-					h_val = 1;
+				//if (m_1 == 0)
+				//	h_val = 1;
 				// else if (tuple_count == 1)
 				// 	m = 2;
 				// else if (m_2 == 0 && m_1 <= 2)
 				// 	h_val = 2;
-				else if (m_2 == 0)
-					h_val = 2;
+				//else if (m_2 == 0)
+				//	h_val = 2;
 				// else if (tuple_count <= 20)
 				// 	m = 5;
 				// else if (m_2 <= 5)
 				// 	h_val= 5;
-				else if (m_2 <= 9)
-					h_val = 3;
-				else if (m_2 <= 99)
-					h_val = 5;
-				else
-					h_val = 9;
+				//else if (m_2 <= 9)
+				//	h_val = 3;
+				//else if (m_2 <= 99)
+				//	h_val = 5;
+				//else
+				//	h_val = 9;
 
 
 			}
