22004ddb1a17da05e9e602b0287dfcf145324046
Giacomo <rosag@student.unimelb.edu.au>
2024-03-27 13:19:36 +1100

    fixes

--- src/planner/bfws/bfws.cxx
+++ src/planner/bfws/bfws.cxx
@@ -332,9 +332,10 @@ void BFWS::solve()
 		bfws_options(search_prob, bfs_engine, max_width, graph);
 		bfs_engine.set_use_h2n(true);
 		// bfs_engine.set_use_h3n(true);
-
-		float bfs_t = do_search(bfs_engine, *prob, plan_stream);
 		bfs_engine.set_budget(1600);
+		
+		float bfs_t = do_search(bfs_engine, *prob, plan_stream);
+
 
 		bfs_engine.delete_heuristics();
 
