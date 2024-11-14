262550960fb300029813ea569d7d0302be64eb54
Giacomo <rosag@student.unimelb.edu.au>
2024-11-15 10:12:52 +1100

    fixed bugs and set budgets to 9999

--- src/engine/gs_bfcs_3h.hxx
+++ src/engine/gs_bfcs_3h.hxx
@@ -332,7 +332,7 @@ namespace aptk
 						m_memory_stop(false), m_alt(false)//, m_h3_only_max_nov(true)
 				{
 
-					m_memory_budget = 6000;
+					m_memory_budget = 9999;
 
 					m_first_h = new First_Heuristic(search_problem);
 					m_second_h = new Second_Heuristic(search_problem);
@@ -775,9 +775,9 @@ namespace aptk
 				{
 					// candidate->partition() = (1000 * candidate->GC()) + candidate->r();
 					if (candidate->parent() != NULL)
-						candidate->partition() = (1000 * candidate->GC()) + 3*candidate->r() + candidate->parent()->olp_hc()+ candidate->parent()->olp_cc();
+						candidate->partition() = (1000 * candidate->GC()) + 2*candidate->r() + candidate->parent()->olp_cn();
 					else
-						candidate->partition() = (1000 * candidate->GC()) + candidate->r();
+						candidate->partition() = (1000 * candidate->GC()) + 2*candidate->r() + 1;
 						
 					m_third_h->eval(candidate, candidate->alt_h1n());
 				}
@@ -787,9 +787,9 @@ namespace aptk
 					// candidate->partition() = (1000 * candidate->GC()) + candidate->r();
 					//adding olp partitions by making r() even for open list 0 and odd for open list 1 of parent
 					if (candidate->parent() != NULL)
-						candidate->partition() = (1000 * candidate->GC()) + 3*candidate->r() + candidate->parent()->olp_hn()+ candidate->parent()->olp_cn();
+						candidate->partition() = (1000 * candidate->GC()) + 2*candidate->r() + candidate->parent()->olp_cc();
 					else
-						candidate->partition() = (1000 * candidate->GC()) +candidate->r();
+						candidate->partition() = (1000 * candidate->GC()) + 2*candidate->r() + 1;
 
 					m_first_h->eval(candidate, candidate->h1n());
 					// candidate->h3n() = candidate->h1n();
@@ -1424,9 +1424,9 @@ namespace aptk
 
 				void set_arity(float v, unsigned g = 0) { 
 					m_first_h->set_arity(1, g); 
-					m_third_h->set_arity(2, g);
+					m_third_h->set_arity(v, g);
 					
-					}
+				}
 				// void set_arity_h3(float v, unsigned g = 0) { m_third_h->set_arity(v, g); }
 				void set_max_novelty(unsigned v)
 				{
